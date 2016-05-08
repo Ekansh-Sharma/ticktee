f839cca9693d94c263e87a6d9672636fd43db93e
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2016-05-08 10:34:46 +0530

    Code refactor

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,11 +10,9 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		@project = Project.find(params[:id])
 	end
 
 	def update
-		@project = Project.find(params[:id])
 		if @project.update(project_params)
 			flash[:notice] = "Project has been updated."
 			redirect_to @project
@@ -26,7 +24,6 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		@project = Project.find(params[:id])
 		if @project.destroy
 			flash[:notice] = "Project has been destroyed."
 		else
@@ -47,7 +44,6 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
 	end
 
 	private
